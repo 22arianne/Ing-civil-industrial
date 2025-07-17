6fba794c3c6e0a55ab5bb00418a43bba991b58b9
22arianne <ariannesantibanez@gmail.com>
2025-07-17 15:56:40 -0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,12 @@
+const grid = document.getElementById('grid');
+
+for (let i = 0; i < 100; i++) {
+  const cell = document.createElement('div');
+  cell.classList.add('cell');
+  
+  cell.addEventListener('click', () => {
+    cell.classList.toggle('active');
+  });
+  
+  grid.appendChild(cell);
+}
